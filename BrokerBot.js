c5cdb7b1b7a7932c6fc411ff66139c2962feaa11
AtomicDonuts <39431742+AtomicDonuts@users.noreply.github.com>
2020-11-13 00:07:18 +0100

    Add files via upload

---
+++ BrokerBot.js
@@ -0,0 +1,64 @@
+var BrokerBot
+if(!BrokerBot) BrokerBot = {};
+BrokerBot.version  = 2.031
+BrokerBot.minigame = Game.ObjectsById[5].minigame
+BrokerBot.minthreshold = 5.00
+BrokerBot.maxthreshold = 105.00
+BrokerBot.goodsById = BrokerBot.minigame.goodsById
+BrokerBot.values = []
+BrokerBot.maxbuy = []
+BrokerBot.Inits = function() {
+  var proceed = false
+  if(BrokerBot.version == Game.version){ proceed = true}
+  if(proceed) {BrokerBot.starter = setInterval(BrokerBot.run,1000);}
+}
+
+BrokerBot.stop = function(){
+  clearInterval(BrokerBot.starter);
+}
+
+BrokerBot.valuefun = function(){
+  for (var i = 0; i < BrokerBot.goodsById.length; i++) {
+    BrokerBot.values[i] = BrokerBot.goodsById[i].val
+  }
+}
+
+BrokerBot.maxbuyfun = function(){
+  for (var i = 0; i < BrokerBot.goodsById.length; i++) {
+    BrokerBot.maxbuy[i] = BrokerBot.minigame.getGoodMaxStock(BrokerBot.goodsById[i])
+  }
+}
+
+BrokerBot.buy = function(){
+  BrokerBot.valuefun()
+  BrokerBot.maxbuyfun()
+  for (var i = 0; i < BrokerBot.goodsById.length; i++) {
+    BrokerBot.goodsById[i].hidden = true
+    BrokerBot.minigame.updateGoodStyle(i)
+  }
+  for (var i = 0; i < BrokerBot.goodsById.length; i++) {
+    if(BrokerBot.values[i] < BrokerBot.minthreshold && BrokerBot.goodsById[i].active){
+      BrokerBot.minigame.buyGood(i,BrokerBot.maxbuy[i])
+      BrokerBot.goodsById[i].hidden = false
+      BrokerBot.minigame.updateGoodStyle(i)
+    }
+  }
+}
+
+BrokerBot.sell = function(){
+  BrokerBot.valuefun()
+  BrokerBot.maxbuyfun()
+  for (var i = 0; i < BrokerBot.goodsById.length; i++) {
+    if(BrokerBot.values[i] > BrokerBot.maxthreshold){
+      BrokerBot.minigame.sellGood(i,BrokerBot.maxbuy[i])
+      BrokerBot.goodsById[i].hidden = true
+      BrokerBot.minigame.updateGoodStyle(i)
+    }
+  }
+}
+
+BrokerBot.run = function(){
+  BrokerBot.buy()
+  BrokerBot.sell()
+  BrokerBot.minigame.checkGraphScale()
+}
